a7bd917f28502d2f7d90c34bd6c6b62a722f4040
Thierry THOUA <titi@lelibre.net>
2021-08-08 12:38:19 +0200

    Update values

--- core/class/AdGuardHome.class.php
+++ core/class/AdGuardHome.class.php
@@ -70,6 +70,117 @@ class AdGuardHome extends eqLogic {
     }
 
     public function postSave() {
+        $refresh = $this->getCmd(null, 'refresh');
+		if (!is_object($refresh)) {
+			$refresh = new AdGuardHomeCmd();
+			$refresh->setName(__('Rafraichir', __FILE__));
+        }
+        
+		$refresh->setEqLogic_id($this->getId());
+		$refresh->setLogicalId('refresh');
+		$refresh->setType('action');
+        $refresh->setSubType('other');
+        $refresh->save();
+
+        $state = $this->getCmd(null, 'state');
+		if (!is_object($state)) {
+			$state = new AdGuardHomeCmd();
+			$state->setName(__('Etat', __FILE__));
+        }
+        
+		$state->setEqLogic_id($this->getId());
+		$state->setLogicalId('state');
+		$state->setType('info');
+        $state->setSubType('binary');
+        $state->setIsVisible(0);
+        $state->setIsHistorized(1);
+        $state->save();
+
+        $on = $this->getCmd(null, 'on');
+		if (!is_object($on)) {
+			$on = new AdGuardHomeCmd();
+			$on->setName(__('On', __FILE__));
+        }
+        
+		$on->setEqLogic_id($this->getId());
+		$on->setLogicalId('on');
+		$on->setType('action');
+        $on->setSubType('other');
+        $on->setIsVisible(1);
+        $on->setValue($state->getId());
+        $on->save();
+
+        $off = $this->getCmd(null, 'off');
+		if (!is_object($off)) {
+			$off = new AdGuardHomeCmd();
+			$off->setName(__('Off', __FILE__));
+        }
+        
+		$off->setEqLogic_id($this->getId());
+		$off->setLogicalId('off');
+		$off->setType('action');
+        $off->setSubType('other');
+        $off->setIsVisible(1);
+        $off->setValue($state->getId());
+        $off->save();
+
+        // Values
+        $processingTime = $this->getCmd(null, 'processingTime');
+		if (!is_object($processingTime)) {
+			$processingTime = new AdGuardHomeCmd();
+			$processingTime->setName(__('Processing Time', __FILE__));
+        }
+        
+		$processingTime->setEqLogic_id($this->getId());
+		$processingTime->setLogicalId('processingTime');
+		$processingTime->setType('info');
+        $processingTime->setSubType('numeric');
+        $processingTime->setIsHistorized(1);
+        $processingTime->setIsVisible(1);
+        $processingTime->save();
+
+        $dnsQueries = $this->getCmd(null, 'dnsQueries');
+		if (!is_object($dnsQueries)) {
+			$dnsQueries = new AdGuardHomeCmd();
+			$dnsQueries->setName(__('DNS Queries', __FILE__));
+        }
+        
+		$dnsQueries->setEqLogic_id($this->getId());
+		$pm25->setLogicalId('dnsQueries');
+		$dnsQueries->setType('info');
+        $dnsQueries->setSubType('numeric');
+        $dnsQueries->setIsHistorized(1);
+        $dnsQueries->setIsVisible(1);
+        $dnsQueries->save();
+
+        $nbrBlockedFiltering = $this->getCmd(null, 'nbrBlockedFiltering');
+		if (!is_object($nbrBlockedFiltering)) {
+			$nbrBlockedFiltering = new AdGuardHomeCmd();
+			$nbrBlockedFiltering->setName(__('DNS Queries Blocked', __FILE__));
+        }
+        
+		$nbrBlockedFiltering->setEqLogic_id($this->getId());
+		$nbrBlockedFiltering->setLogicalId('nbrBlockedFiltering');
+		$nbrBlockedFiltering->setType('info');
+        $nbrBlockedFiltering->setSubType('numeric');
+        $nbrBlockedFiltering->setIsHistorized(1);
+        $nbrBlockedFiltering->setIsVisible(1);
+        $nbrBlockedFiltering->save();
+
+        $nbrBlockedPercentage = $this->getCmd(null, 'nbrBlockedPercentage');
+		if (!is_object($nbrBlockedPercentage)) {
+			$nbrBlockedPercentage = new AdGuardHomeCmd();
+			$nbrBlockedPercentage->setName(__('% Blocked', __FILE__));
+        }
+        
+		$nbrBlockedPercentage->setEqLogic_id($this->getId());
+		$nbrBlockedPercentage->setLogicalId('nbrBlockedPercentage');
+		$nbrBlockedPercentage->setType('info');
+        $nbrBlockedPercentage->setSubType('numeric');
+        $nbrBlockedPercentage->setIsHistorized(1);
+        $nbrBlockedPercentage->setIsVisible(1);
+        $nbrBlockedPercentage->setConfiguration('maxValue', 100);
+        $nbrBlockedPercentage->save();
     }
 
     public function preUpdate() {
@@ -92,6 +203,19 @@ class AdGuardHome extends eqLogic {
         $cmd = 'sudo python3 '.dirname(__FILE__) . '/../../resources/stats.py '. $this->getConfiguration("IP") .' '. $this->getConfiguration("Port") .' '. $this->getConfiguration("Username") .' '. $this->getConfiguration("Password");
         $result = shell_exec($cmd);
         log::add('AdGuardHome', 'debug', $result);
+        if($result != "Error"){
+            $values = explode(";", $result);
+            $stateCmd = $this->getCmd(null, 'state');
+            $stateCmd->event($values[0]);
+            $processingTimeCmd = $this->getCmd(null, 'processingTime');
+            $processingTimeCmd->event($values[1]);
+            $dnsQueriesCmd = $this->getCmd(null, 'dnsQueries');
+            $dnsQueriesCmd->event($values[2]);
+            $nbrBlockedFilteringCmd = $this->getCmd(null, 'nbrBlockedFiltering');
+            $nbrBlockedFilteringCmd->event($values[3]);
+            $nbrBlockedPercentageCmd = $this->getCmd(null, 'nbrBlockedPercentage');
+            $nbrBlockedPercentageCmd->event($values[4]);
+        }
     }
 }
 
