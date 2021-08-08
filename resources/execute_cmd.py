16e45430220b88a48af1d92b0316c570abd0da01
Thierry THOUA <titi@lelibre.net>
2021-08-08 13:16:05 +0200

    Fix off on command

--- resources/execute_cmd.py
+++ resources/execute_cmd.py
@@ -6,15 +6,12 @@ from adguardhome import AdGuardHome
 async def main(argv):
     try:
        async with AdGuardHome(argv[0], port=argv[1], username=argv[2], password=argv[3]) as adguard:
-          if(argv[4] == 1)
-          {
+          if argv[4] == "1":
             await adguard.enable_protection()
             print("Done")
-          }
-          else{
+          else:
             await adguard.disable_protection()
             print("Done")
-          }
     except:
        print("Error")
 
